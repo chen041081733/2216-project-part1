1d06a23a09bfe5108dfd0688ddd4b1b27c41b847
Zhitao Chen <chen0912@algonquinlive.com>
2025-02-14 21:47:26 -0500

    Add files via upload

--- part1_streamlit.py
+++ part1_streamlit.py
@@ -1,3 +1,12 @@
+
+import os
+import sys
+
+#os.path.join(os.path.dirname(__file__), '..')：返回 src 的上一级目录，sys.path.append(...)：这样就可以让 Python 在 2216 project-part1 目录下查找 src 目录里的模块。
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import logging
+
+
 import pandas as pd
 import numpy as np
 import streamlit as st
@@ -5,21 +14,21 @@ import pickle
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
-import os
-import sys
+
 from src.model_select import data_preparation, Linear_regression_prediction, decision_tree_prediction, Random_Forest_prediction, pickle_model
 
-#os.path.join(os.path.dirname(__file__), '..')：返回 src 的上一级目录，sys.path.append(...)：这样就可以让 Python 在 2216 project-part1 目录下查找 src 目录里的模块。
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
-#如果 sys.path.append(...) 写在 import 之后，Python 还是找不到 src，所以一定要在 import 之前添加路径。
-from src.model_select import data_preparation, Linear_regression_prediction, decision_tree_prediction, Random_Forest_prediction, pickle_model
+try:
 
-#file_path = r"C:\algonquin\2025W\2216_ML\2216_project\2216_project-part1\data\cleaned_df.csv"
-file_path = "https://raw.githubusercontent.com/chen041081733/2216-project-part1/main/data/cleaned_df.csv"
-
-x_train, x_test, y_train, y_test = data_preparation(file_path)
-
+    #file_path = r"C:\algonquin\2025W\2216_ML\2216_project\2216_project-part1\data\cleaned_df.csv"
+    file_path = "https://raw.githubusercontent.com/chen041081733/2216-project-part1/main/data/cleaned_df.csv"
+    x_train, x_test, y_train, y_test = data_preparation(file_path)
+    logging.info("data load successfully")
+except Exception as e:
+    logging.error(f"data load fail: {e}")
+    st.error("data load fail, please check again。")
+    st.stop()
 
 
 
@@ -41,11 +50,11 @@ basement = st.selectbox("basement", ["Y", "N"])
 # property type selection
 property_type = st.selectbox("property_type", ["Bunglow", "Condo"])
 
-# 转换 'basement' 为二进制
-basement_binary = 1 if basement == "是" else 0
+# transfer 'basement' to binary
+basement_binary = 1 if basement == "Y" else 0
 
-# 用用户输入的特征创建一个预测数据点
-if st.button("pridiction of house price"):
+# input for price prediction 
+if st.button("prediction of house price"):
     input_data = pd.DataFrame({
         'year_sold': [year_sold],
         'property_tax': [property_tax],
